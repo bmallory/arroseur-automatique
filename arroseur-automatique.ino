edec89313b28efe3a10d48e1899381db1b0e3e01
cyril cheney <cyril.cheney@lagardere-active.com>
2017-03-15 18:06:02 +0100

    lecture de l'analog plutot

--- arroseur-automatique.ino
+++ arroseur-automatique.ino
@@ -3,45 +3,55 @@
 
 
 int ledPin = 13; // LED connected to digital pin 13
-int inPin = 7;   // pushbutton connected to digital pin 7
-int val = 0;   // variable to store the read value
+int secondledPin = 7; // LED connected to digital pin 13
+int inPin = A0;   // pushbutton connected to digital pin 7
+int valRaw = 0;   // variable to store the read value
+int valReal = 0;   // variable to store the read value
 
 int relayPin = 8;
 
 void setup()
 {
-  pinMode(13,OUTPUT);
+  pinMode(ledPin,OUTPUT);
+  pinMode(secondledPin,OUTPUT);
+
   pinMode(relayPin,OUTPUT);
   digitalWrite(relayPin, LOW);
   pinMode(inPin, INPUT);      // sets the digital pin 7 as input
-  
+
 }
 
 void loop()
 {
-  digitalWrite(13,HIGH);
+  digitalWrite(ledPin,HIGH);
+  digitalWrite(secondledPin,HIGH);
   delay(20);
-  digitalWrite(13,LOW);
-  val = digitalRead(inPin);   // read the input pin
-  if(val == HIGH){
+  digitalWrite(ledPin,LOW);
+  digitalWrite(secondledPin,LOW);
+  valRaw = analogRead(pin);(inPin);   // read the input pin
+  valReal = map(valRaw, 1023, 0, 0, 100 );
+
+  if(valReal < 80 ){ //80% d'humidité
     waterthatshite();
   }
-  
-  
+
+
     // ATmega328P, ATmega168
-/*  LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, 
+/*  LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF,
                 SPI_OFF, USART0_OFF, TWI_OFF);
-           */     
-                
- LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, 
+           */
+
+ LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF,
                 SPI_OFF, USART0_OFF, TWI_OFF);
-  
+
 }
 
 void waterthatshite()
 {
   digitalWrite(relayPin, HIGH);
+  digitalWrite(secondledPin,HIGH);
   delay(60000); //1 minute
   digitalWrite(relayPin, LOW);
+  digitalWrite(secondledPin,LOW);
 }
 
